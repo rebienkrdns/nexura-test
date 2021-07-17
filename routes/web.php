6d9834dbe3ce0c23bae9a8a34d1f8bf0885c8528
Neiber Cardenas <rebienkrdns@gmail.com>
2021-07-16 21:25:20 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return \Illuminate\Support\Facades\Redirect::to('/empleados');
+});
+
 Route::resource('/empleados', GestionController::class);
